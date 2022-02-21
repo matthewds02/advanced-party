f3655cc870f5053510b6c6ccc0b63067ba70091d
Matthew De Schepper <r0903037@student.thomasmore.be>
2022-02-21 17:39:23 +0100

    adding artist details and buttons

--- src/main/java/be/thomasmore/party/controllers/ArtistController.java
+++ src/main/java/be/thomasmore/party/controllers/ArtistController.java
@@ -6,11 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class ArtistController {
     @Autowired
     private ArtistRepository artistRepository;
+
+    @GetMapping({"/artistdetails", "/artistdetails/{id}"})
+    public String artistDetails(Model model, @PathVariable(required = false) Integer id) {
+        if (id==null) return "artistdetails";
+        Optional<Artist> optionalVenue = artistRepository.findById(id);
+        if (optionalVenue.isPresent()) {
+            model.addAttribute("artist", optionalVenue.get());
+        }
+        return "artistdetails";
+    }
 
     @GetMapping("/artistlist")
     public String venueList(Model model) {
